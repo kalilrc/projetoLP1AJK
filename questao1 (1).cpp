f8574ae83b5ce238ac40650f6f8422668229f968
kalilrc <kalil_xp@hotmail.com>
2017-03-28 12:03:01 -0300

    Add files via upload

---
+++ questao1 (1).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "relogio.h"
+#include "relogioteste.h"
+
+using namespace std;
+
+int main(){
+    RelogioTeste conjunto1;
+
+    conjunto1.main();
+
+
+}
---
+++ questao2 (1).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "TelevisaoTeste.h"
+#include "televisao.h"
+
+
+using namespace std;
+
+int main(){
+
+    TelevisaoTeste primeiroTeste;
+
+    primeiroTeste.main();
+
+}
